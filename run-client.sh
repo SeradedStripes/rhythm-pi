9c5f6841ce284794b65e34412011f4a0e29f9452
SeradedStripes <seradedstripes@gmail.com>
2026-01-18 01:13:56 +0200

    made other run scripts stop its processes fuly

--- run-client.sh
+++ run-client.sh
@@ -1,2 +1,3 @@
 #!/bin/sh
 cargo run -p rhythm-pi-client --release
+pkill rhythm-pi-client || true
--- run-server.sh
+++ run-server.sh
@@ -1,2 +1,3 @@
 #!/bin/sh
 cargo run -p rhythm-pi-server --bin rhythm-pi-server --release
+pkill rhythm-pi-server || true
